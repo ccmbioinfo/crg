80d7aaa7db08a5fe59ae75a7f6e6e1f0fdf51bb7
dennis-kao <d.kao@live.com>
2019-10-07 15:36:08 -0400

    Refactored parsing VCF logic. Grouping on INS works properly now that END is computed from SVLEN

--- compare.sv.calls.py
+++ compare.sv.calls.py
@@ -9,63 +9,55 @@ from pybedtools import BedTool
 
 sample_list = []
 
-def list2string(col):
-	return col.apply(lambda x: ', '.join(x) if isinstance(x, list) else x)
+def parse_vcf(vcf):
+	print("Parsing %s" % vcf)
 
-def parse_vcfs(vcfs):
-
-	ann_dfs = []
+	vcf_dict = allel.read_vcf(vcf, ['*'])
+	name = os.path.basename(vcf).replace(".vcf", "")
 	
-	for vcf in vcfs:
-		print("Parsing %s" % vcf)
+	for key in list(vcf_dict.keys()):
+		if key not in ['variants/CHROM','variants/POS','variants/END', 'variants/SVTYPE', 'variants/SVLEN',]:
+			vcf_dict.pop(key)
+	
+	df = pd.DataFrame(vcf_dict)
+	df['SAMPLE'] = name
+	sample_list.append(name)
 
-		vcf_dict = allel.read_vcf(vcf, ['*'])
-		name = os.path.basename(vcf).replace(".vcf", "")
-		
-		for key in list(vcf_dict.keys()):
-			if key not in ['variants/CHROM','variants/POS','variants/END', 'variants/SVTYPE', 'variants/SVLEN',]:
-				vcf_dict.pop(key)
-		
-		df = pd.DataFrame(vcf_dict)
-		df['SAMPLE'] = name
-		sample_list.append(name)
+	# throw out BND rows
+	df = df[df['variants/SVTYPE'] != "BND"]
 
-		# throw out BND rows
-		df = df[df['variants/SVTYPE'] != "BND"]
+	if 'variants/END' not in df.columns:
+		df['variants/END'] = np.nan
+	elif 'variants/SVLEN' not in df.columns:
+		# Delly VCF
+		df['variants/SVLEN'] = df['variants/END'] - df['variants/POS']
+	elif 'variants/END' not in df.columns and 'variants/SVLEN' not in df.columns:
+		raise Exception("VCF does not have a SVLEN or END field. Are you sure this is a structural variant VCF?")
 
-		if 'variants/END' not in df.columns:
-			df['variants/END'] = np.nan
-		elif 'variants/SVLEN' not in df.columns:
-			# Delly VCF
-			df['variants/SVLEN'] = df['variants/END'] - df['variants/POS']
-		elif 'variants/END' not in df.columns and 'variants/SVLEN' not in df.columns:
-			raise Exception("VCF does not have a SVLEN or END field. Are you sure this is a structural variant VCF?")
+	# compute END for ALU-insertion,LINE1-insertion,SVA-insertion SVTYPE rows
+	no_end_svtypes = (df['variants/SVTYPE'] == "ALU") | (df['variants/SVTYPE'] == "LINE1") | (df['variants/SVTYPE'] == "SVA") |  (df['variants/SVTYPE'] == "INS")
+	if len(df.loc[no_end_svtypes, :]) != 0:
+		df.loc[no_end_svtypes, ['variants/END']] = df['variants/POS'] + df['variants/SVLEN']
 
-		# compute END for ALU-insertion,LINE1-insertion,SVA-insertion SVTYPE rows
-		no_end_svtypes = (df['variants/SVTYPE'] == "ALU") | (df['variants/SVTYPE'] == "LINE1") | (df['variants/SVTYPE'] == "SVA") 
-		if len(df.loc[no_end_svtypes, :]) != 0:
-			df.loc[no_end_svtypes, ['variants/END']] = df['variants/POS'] + df['variants/SVLEN']
+	# workaround for START > END so BedTool doesn't freak out with MalformedBedError
+	# START > END is the case for TRV, INV, DEL
+	start_gt_end = df['variants/END'] < df['variants/POS']
+	if len(df.loc[start_gt_end, :]) > 0:
+		df.loc[start_gt_end, ['variants/END','variants/POS']] = df.loc[start_gt_end, ['variants/POS','variants/END']].values
+		df['variants/POS'] = df['variants/POS'].astype(int)
+		df['variants/END'] = df['variants/END'].astype(int)
+		df = df.drop_duplicates()
 
-		# workaround for START > END so BedTool doesn't freak out with MalformedBedError
-		# START > END is the case for TRV, INV, DEL
-		start_gt_end = df['variants/END'] < df['variants/POS']
-		if len(df.loc[start_gt_end, :]) > 0:
-			df.loc[start_gt_end, ['variants/END','variants/POS']] = df.loc[start_gt_end, ['variants/POS','variants/END']].values
-			df['variants/POS'] = df['variants/POS'].astype(int)
-			df['variants/END'] = df['variants/END'].astype(int)
-			df = df.drop_duplicates()
+	# compute SVLEN for empty records (SVTYPE=CNV)
+	df.loc[(df['variants/SVLEN'] == '') | (df['variants/SVLEN'] == '.'), 'variants/SVLEN'] = df['variants/END'] - df['variants/POS']
 
-		# compute SVLEN for empty records (SVTYPE=CNV)
-		df.loc[(df['variants/SVLEN'] == '') | (df['variants/SVLEN'] == '.'), 'variants/SVLEN'] = df['variants/END'] - df['variants/POS']
+	# typecast potential floats to ints
+	df[["variants/POS", "variants/END", 'variants/SVLEN']] = df[["variants/POS", "variants/END", 'variants/SVLEN']].astype(int)
 
-		# typecast potential floats to ints (due to implicit typecasting)
-		df[["variants/POS", "variants/END", 'variants/SVLEN']] = df[["variants/POS", "variants/END", 'variants/SVLEN']].astype(int)
+	return df
 
-		#print(df)
-
-		ann_dfs.append(df)
-
-	ann_df = pd.concat(ann_dfs)
+def combine_vcf_df(vcf_dfs):
+	ann_df = pd.concat(vcf_dfs)
 	ann_df.columns = ann_df.columns.str.replace('variants/', '')
 	ann_df = ann_df[["CHROM", "POS", "END", "SVTYPE", "SAMPLE"]]
 
@@ -121,12 +113,14 @@ def add_interval(master_df, ref_interval, samp_interval):
 	row["%s_SV_DETAILS" % samp_name].append('{}:{}-{}:{}'.format(samp_chr, samp_start, samp_end, samp_svtype))
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Generates a report to compare structural variant calls between multiple callers (and a truth dataset) through reciprocal overlap')
+	parser = argparse.ArgumentParser(description='Generates a report to compare structural variant calls between multiple callers (or a truth dataset) through reciprocal overlap')
 	parser.add_argument('-i', type=str, nargs='+', help='VCF files containing structural variants, e.g. -i 180_230.vcf 180_231.vcf', required=True)
 	parser.add_argument('-r_overlap', type=float, help='Percent reciprocal overlap required to consider 2 calls to be equivalent in decimal', required=True)
 	parser.add_argument('-o', type=str, help='TSV output file name, default = overlapping.calls.tsv', default="overlapping.calls.tsv")
 	args = parser.parse_args()
 
-	bedtool = parse_vcfs(args.i)
+	vcf_dfs = [parse_vcf(vcf) for vcf in args.i]
+	bedtool = combine_vcf_df(vcf_dfs)
+
 	df = group_sv(bedtool, args.r_overlap)
 	df.to_csv(args.o, sep="\t")
