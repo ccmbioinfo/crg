030693f01a0c01311870c5429e57c2764a187ca0
dennis-kao <d.kao@live.com>
2020-02-13 11:27:38 -0500

    Added a proper outfile file to snpeff script

--- crg.snpeff.sh
+++ crg.snpeff.sh
@@ -6,7 +6,8 @@
 #PBS -l vmem=50g,mem=50g
 
 IN=$1
+OUT="${IN}.snpeff.vcf"
 
 /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/anaconda/bin/snpEff -Xms750m -Xmx20g  -i vcf -o vcf -dataDir /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/snpeff \
 GRCh37.75 \
-${IN}
+${IN} > ${OUT}
