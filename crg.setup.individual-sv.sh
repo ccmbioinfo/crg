88e13d4c3d843a2b64c1d58e2f900d1218fa0ace
aarthi-mohan <aarthimohan11@gmail.com>
2020-06-24 15:41:57 -0400

    removed for loop, added samples argument

--- crg.setup.individual-sv.sh
+++ crg.setup.individual-sv.sh
@@ -4,29 +4,26 @@
 #PBS -d .
 #PBS -l vmem=21g,mem=21g
 
-for f in ${family_id}*; 
-do
-	echo $f
-	if [ -d $f ]
-	then 
-		cd $f/${family_id}/input;
-		ln -s ../../../../remove_decoys/${f}.no_decoy_reads.bam $f.bam; 
-		cd ../../..;
-	else
-		echo "Decoy file not found"
-	fi
+#arguments passed: family_id and sample 
 
-done
+echo "sample = $sample";
+if [ -d $sample ]
+then
+	cd $sample/${family_id}/input;
+	ln -s ../../../../remove_decoys/${sample}.no_decoy_reads.bam $sample.bam; 
+	cd ../../..;
+else
+	echo "Decoy file not found"
+fi
 
-for f in ${family_id}*; 
-do 
-	if [ -d $f ]
-	then
-		cd $f;
-		pwd
-		~/crg/crg.prepare_bcbio_run.sh ${family_id} sv;
-		cd ..;
-	else
-		echo "Participant Folder Not Found."
-	fi
-done
+
+if [ -d $sample ]
+then
+	cd $sample;
+	echo "current directory: `pwd`";
+	~/crg/crg.prepare_bcbio_run.sh ${family_id} sv;
+	cd ..;
+else
+	echo "Participant Folder Not Found."
+fi
+
