2235a6cf6d7bb018ae52b568f3bdc994dfb55a00
Madeline Couse <mhcouse@gmail.com>
2020-07-08 09:15:38 -0400

    script to submit jobs for  merging, annotating, and scoring svs

---
+++ crg.merge.annotate.sv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# usage: crg.merge.annotate.sv.sh <family>
+# run from within family/bcbio-sv/ 
+
+FAMILY=$1
+
+#run metasv on each sample
+for f in $FAMILY_*/$FAMILY/final/$FAMILY* 
+do 
+	cd $f
+	SAMPLE="$(echo $f | cut -d'/' -f1)"
+	metasv_jobs+=($(qsub ~/crg/metasv.pbs -v SAMPLE=$SAMPLE,FAMILY=$FAMILY))
+	cd ../../../..
+done
+metasv_string=$( IFS=$':'; echo "${metasv_jobs[*]}" )
+
+#run snpeff on each sample
+for f in $FAMILY_*/$FAMILY/final/$FAMILY*
+do
+	SAMPLE="$(echo $f | cut -d'/' -f1)"
+	snpeff_jobs+=($(qsub ~/crg/crg.snpeff.sh -F $f/$SAMPLE/*metasv.filtered.vcf.gz -W depend=afterany:"${metasv_string}"))
+done
+snpeff_string=$( IFS=$':'; echo "${snpeff_jobs[*]}" )
+
+#run svscore on each sample
+for f in $FAMILY_*/$FAMILY/final/$FAMILY*
+do
+	SAMPLE="$(echo $f | cut -d'/' -f1)"
+	snpeff_jobs+=($(qsub ~/crg/crg.svscore.sh -F $f/$SAMPLE/*snpeff.vcf -W depend=afterany:"${snpeff_string}"))
+done
+
+echo "Merging SVs with metaSV, annotating with snpeff, and scoring with svscore..."
+
+
