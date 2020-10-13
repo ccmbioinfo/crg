7c3aa2a931b005a11ac5024ea28e417ea1dd1c4c
aarthi-mohan <aarthimohan11@gmail.com>
2020-10-13 13:47:59 -0400

    script to automate panel reports

---
+++ panel_report.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#usage: sh panel_report.sh <family> <path/to/ensemble.vcf.gz>
+#run this from base <family> folder
+
+function panel_report
+{
+
+if [ ! -d "${dir}/${family}" ]; then
+	mkdir ${dir}/${family}
+fi;
+
+panel_vcf="${dir}/${family}/${family}-ensemble-annotated-decomposed.vcf.gz";
+echo "$panel_vcf, $ensemble, $bed";
+
+if [ ! -f "$panel_vcf" ]; then
+	echo "generating ${panel_vcf}";
+	bedtools intersect -header -a $ensemble -b $bed > ${panel_vcf}
+fi;
+
+cd ${dir}
+echo "report generation for $dir"
+sh ~/crg/generate_reports.sh ${family} "wgs"
+cd ..
+
+}
+
+family=$1;
+ensemble=$2; #ensemble annotated vcf.gz from small-variant
+
+#panel
+dir="panel";
+bed="genes/${family}.bed"
+if [ ! -d genes ] || [ ! -f $bed ]; then
+	echo "folder: genes or file: genes/${family}.bed not found. Exiting!";
+	exit
+fi;
+panel_report
+
+#panel flank
+~/crg/crg.flank.sh $bed > genes/${familyid}.flank.100k.bed
+dir="panel-flank100k";
+bed="genes/${familyid}.flank.100k.bed";
+panel_report
