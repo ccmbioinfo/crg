f01122ea5ea551009e7e9513eb84b545de609f54
aarthi-mohan <aarthimohan11@gmail.com>
2020-11-09 18:08:34 -0500

    scripts for integrating STR reports

---
+++ get_XY.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+x=`samtools idxstats $1 | grep "X"`;
+y=`samtools idxstats $1 | grep "Y"`;
+xcov=`echo $x | awk '{ printf("%0.5f", $3/$2); }'`;
+ycov=`echo $y | awk '{ printf("%0.5f", $3/$2); }'`;
+
+rat=$(echo "scale=4; ${xcov}/${ycov}" | bc)
+if (( $(echo "$rat > 5.0" | bc -l) )); then
+    sex=female
+else
+    sex=male
+fi
+echo "$sex"
