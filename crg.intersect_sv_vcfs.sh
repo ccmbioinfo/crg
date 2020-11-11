6412ab6ad7f08445fc9523ad1f51a5b82964f445
Madeline Couse <mhcouse@gmail.com>
2020-11-11 10:24:36 -0500

    filter metaSV after snpeff and svscore annotation and produce unfiltered report

--- crg.intersect_sv_vcfs.sh
+++ crg.intersect_sv_vcfs.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-#PBS -l walltime=2:00:00,nodes=1:ppn=1
+#PBS -l walltime=4:00:00,nodes=1:ppn=1
 #PBS -joe .
 #PBS -d .
 #PBS -l vmem=10g,mem=10g
 
-if [ -z $1 ]; then
-	echo "Specify family ID as first arguement to script"
-	exit
-fi
+# if [ -z $FAMILY ]; then
+# 	echo "Specify family ID as first arguement to script"
+# 	exit
+# fi
 
-if [ -z $2 ]; then
-	echo "Specify type (filter or unfiltered) as second argument to script"
-	exit
-fi
+# if [ -z $type ]; then
+# 	echo "Specify type (filter or unfiltered) as second argument to script"
+# 	exit
+# fi
 
 TODAY=`date +%Y-%m-%d`
-FAMILY_ID=$family
-TYPE=$type
-OUT=${FAMILY_ID}.${TYPE}.wgs.sv.${TODAY}.tsv
+FAMILY=$1
 
 GENE_DATA=${HOME}/gene_data
 HGMD=${GENE_DATA}/HGMD_2018/hgmd_pro.db
@@ -49,11 +47,38 @@ elif [[ "$OSTYPE" == "linux"* ]]; then
 	PY=python
 fi
 
-echo "${PY} ${HOME}/crg/crg.intersect_sv_vcfs.py -protein_coding_genes=${PROTEIN_CODING_GENES} -exon_bed=${EXON_BED} -hgmd=${HGMD} -hpo=${HPO} -exac=${EXAC} -omim=${OMIM} -biomart=${BIOMART} -gnomad=${GNOMAD} -sv_counts ${MSSNG_MANTA_COUNTS} ${MSSNG_LUMPY_COUNTS} -o=${OUT} -i ${IN_FILES}"
-${PY} ${HOME}/crg/crg.intersect_sv_vcfs.py -protein_coding_genes=${PROTEIN_CODING_GENES} -exon_bed=${EXON_BED} -hgmd=${HGMD} -hpo=${HPO} -exac=${EXAC} -omim=${OMIM} -biomart=${BIOMART} -gnomad=${GNOMAD} -sv_counts ${MSSNG_MANTA_COUNTS} ${MSSNG_LUMPY_COUNTS} -o=${OUT} -i ${IN_FILES}
+for FILE in $IN_FILES
+do
+	bcftools filter -e "((NUM_SVTOOLS = 1 && ABS(SVLEN)>50000) || (NUM_SVTOOLS = 1 && ABS(SVLEN)<4000 && BA_FLANK_PERCENT>80) || (NUM_SVTOOLS = 1 && ABS(SVLEN)<4000 && BA_NUM_GOOD_REC=0) || (ABS(SVLEN)<4000 && BA_NUM_GOOD_REC>2)) || FILTER='LowQual'" \
+		-O z \
+		-o ${FILE}.filtered.gz \
+		$FILE 
+done
 
-${HOME}/crg/cap_report.py ${OUT}
+FILTERED_FILES=`ls ${FAMILY}_*/${FAMILY}/final/${FAMILY}*/${FAMILY}*/*filtered.gz | tr '\n' ' '`
+
+for FILE in $FILTERED_FILES
+do
+	tabix $FILE
+done
+
+#make filtered report
+echo "${PY} ${HOME}/crg/crg.intersect_sv_vcfs.py -protein_coding_genes=${PROTEIN_CODING_GENES} -exon_bed=${EXON_BED} -hgmd=${HGMD} -hpo=${HPO} -exac=${EXAC} -omim=${OMIM} -biomart=${BIOMART} -gnomad=${GNOMAD} -sv_counts ${MSSNG_MANTA_COUNTS} ${MSSNG_LUMPY_COUNTS} -o=${FAMILY}.wgs.sv.${TODAY}.tsv -i ${FILTERED_FILES}"
+${PY} ${HOME}/crg/crg.intersect_sv_vcfs.py -protein_coding_genes=${PROTEIN_CODING_GENES} -exon_bed=${EXON_BED} -hgmd=${HGMD} -hpo=${HPO} -exac=${EXAC} -omim=${OMIM} -biomart=${BIOMART} -gnomad=${GNOMAD} -sv_counts ${MSSNG_MANTA_COUNTS} ${MSSNG_LUMPY_COUNTS} -o=${FAMILY}.wgs.sv.${TODAY}.tsv -i ${FILTERED_FILES}
+
+${HOME}/crg/cap_report.py ${FAMILY}.wgs.sv.${TODAY}.tsv
 
 if [[ "$OSTYPE" == *"darwin"* ]]; then
-	open -a 'Microsoft Excel' ${OUT}
+	open -a 'Microsoft Excel' ${FAMILY}.wgs.sv.${TODAY}.tsv
+fi
+
+
+#make unfiltered report
+echo "${PY} ${HOME}/crg/crg.intersect_sv_vcfs.py -protein_coding_genes=${PROTEIN_CODING_GENES} -exon_bed=${EXON_BED} -hgmd=${HGMD} -hpo=${HPO} -exac=${EXAC} -omim=${OMIM} -biomart=${BIOMART} -gnomad=${GNOMAD} -sv_counts ${MSSNG_MANTA_COUNTS} ${MSSNG_LUMPY_COUNTS} -o=${FAMILY}.unfiltered.wgs.sv.${TODAY}.tsv -i ${IN_FILES}"
+${PY} ${HOME}/crg/crg.intersect_sv_vcfs.py -protein_coding_genes=${PROTEIN_CODING_GENES} -exon_bed=${EXON_BED} -hgmd=${HGMD} -hpo=${HPO} -exac=${EXAC} -omim=${OMIM} -biomart=${BIOMART} -gnomad=${GNOMAD} -sv_counts ${MSSNG_MANTA_COUNTS} ${MSSNG_LUMPY_COUNTS} -o=${FAMILY}.unfiltered.wgs.sv.${TODAY}.tsv -i ${IN_FILES}
+
+${HOME}/crg/cap_report.py ${FAMILY}.unfiltered.wgs.sv.${TODAY}.tsv
+
+if [[ "$OSTYPE" == *"darwin"* ]]; then
+	open -a 'Microsoft Excel' ${FAMILY}.unfiltered.wgs.sv.${TODAY}.tsv
 fi
--- crg.merge.annotate.sv.sh
+++ crg.merge.annotate.sv.sh
@@ -14,38 +14,23 @@ do
 done
 metasv_string=$( IFS=$':'; echo "${metasv_jobs[*]}" )
 
-#run snpeff on each sample on both filtered and unfiltered metasv output
-#filtered metasv
+#run snpeff on each sample
 for f in $FAMILY_*/$FAMILY/final/$FAMILY*
 do
 	SAMPLE="$(echo $f | cut -d'/' -f1)"
-	snpeff_jobs+=($(qsub ~/crg/crg.snpeff.sh -F $f/$SAMPLE/*metasv.filtered.vcf.gz -W depend=afterany:"${metasv_string}"))
+	snpeff_jobs+=($(qsub ~/crg/crg.snpeff.sh -F $f/$SAMPLE/variants.vcf.gz -W depend=afterany:"${metasv_string}"))
 done
 snpeff_string=$( IFS=$':'; echo "${snpeff_jobs[*]}" )
-#unfiltered metasv
-for f in $FAMILY_*/$FAMILY/final/$FAMILY*
-do
-	SAMPLE="$(echo $f | cut -d'/' -f1)"
-	snpeff_unfiltered_jobs+=($(qsub ~/crg/crg.snpeff.sh -F $f/$SAMPLE/variants.vcf.gz -W depend=afterany:"${metasv_string}"))
-done
-snpeff_unfiltered_string=$( IFS=$':'; echo "${snpeff_unfiltered_jobs[*]}" )
 
-#run svscore on each sample on both filtered and unfiltered metasv output
+
+#run svscore on each sample 
 for f in $FAMILY_*/$FAMILY/final/$FAMILY*
 do
 	SAMPLE="$(echo $f | cut -d'/' -f1)"
-	svscore_jobs+=($(qsub ~/crg/crg.svscore.sh -F $f/$SAMPLE/*filtered.vcf.gz.snpeff.vcf -W depend=afterany:"${snpeff_string}"))
+	svscore_jobs+=($(qsub ~/crg/crg.svscore.sh -F $f/$SAMPLE/variants.vcf.gz.snpeff.vcf -W depend=afterany:"${snpeff_string}"))
 done
 svscore_string=$( IFS=$':'; echo "${svscore_jobs[*]}" )
-#unfiltered metasv
-for f in $FAMILY_*/$FAMILY/final/$FAMILY*
-do
-	SAMPLE="$(echo $f | cut -d'/' -f1)"
-	svscore_unfiltered_jobs+=($(qsub ~/crg/crg.svscore.sh -F $f/$SAMPLE/variants.vcf.gz.snpeff.vcf -W depend=afterany:"${snpeff_unfiltered_string}"))
-done
-svscore_unfiltered_string=$( IFS=$':'; echo "${svscore_unfiltered_jobs[*]}" )
 echo "Merging SVs with metaSV, annotating with snpeff, scoring with svscore, and creating report..."
 
-qsub ~/crg/crg.intersect_sv_vcfs.sh -v family=$FAMILY,type=filtered -W depend=afterany:"${svscore_string}"
-qsub ~/crg/crg.intersect_sv_vcfs.sh -v family=$FAMILY,type=unfiltered -W depend=afterany:"${svscore_unfiltered_string}"
+qsub ~/crg/crg.intersect_sv_vcfs.sh -F $FAMILY -W depend=afterany:"${svscore_string}"
 
