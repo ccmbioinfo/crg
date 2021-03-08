a44cbe1b18583597ce2e7e1e4e95a597a552f864
aarthi-mohan <aarthimohan11@gmail.com>
2021-03-08 14:27:51 -0500

    added precomputed EH GT for 1000G(v1.0 catalog); script for per-family report + fixed bugs

--- crg.eh.sh
+++ crg.eh.sh
@@ -4,16 +4,34 @@
 #PBS -d .
 
 ref=/hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37d5/seq/GRCh37d5.fa;
-catalog=/hpf/largeprojects/ccmbio/arun/C4Rare/Tandem_repeat_disease_loci.hg19.json;
-scripts=/hpf/largeprojects/ccmbio/arun/C4Rare/scripts/scripts;
-tr_annot=/hpf/largeprojects/ccmbio/arun/C4Rare/scripts/scripts/TR_disease_loci_for_adding_to_col_headings.tsv;
+catalog=/hpf/largeprojects/ccmbio_ephemeral/ExpansionHunter/tandem_repeat_disease_loci_v1.1.hg19.masked.json;
+scripts=/hpf/largeprojects/ccmbio/aarthi/proj_CHEO/CRG/str;
+
+#original annotation file from Brett:/hpf/largeprojects/ccmbio_ephemeral/ExpansionHunter/tandem_repeat_disease_loci_v1.1.tsv
+#em_dashes from above tsv were causing issues when splitting annotation in eh_sample_report.py; so manually replaced those with hyphen and stored in dir below
+tr_annot=/hpf/largeprojects/ccmbio/aarthi/proj_CHEO/CRG/str/tandem_repeat_disease_loci_v1.1.tsv;
 
 
 if [ -z $family ]; then family=$1; fi;
-outdir="str/expansion_hunter";
+
+bcbio="${family}/bcbio-align/${family}/final/${family}_*";
+if [ -z "`ls ${bcbio}/${family}_*-ready.bam`" ]; then 
+    if [ -z "`ls ${family}/${family}*-ready.bam`" ]; then
+        echo "BAM files for $family not found inside bcbio-align/ and $family/. Exiting!";
+        exit
+    else
+        dir=$family;
+    fi;
+else
+    dir=$bcbio;
+fi;
+
+outdir="${family}/str/expansion_hunter";
+echo "${outdir}, ${dir}, $family";
 
 module load ExpansionHunter/3.0.1
-for i in `ls bcbio-align/${family}/final/${family}_*/${family}_*-ready.bam`; do
+for i in `ls $dir/${family}*-ready.bam`; do
+    echo "bam = $i";
     if [ ! -d $outdir ]; then
         mkdir -p $outdir;
     fi;
@@ -21,24 +39,33 @@ for i in `ls bcbio-align/${family}/final/${family}_*/${family}_*-ready.bam`; do
     reads=`readlink -f $i`;
     eh_prefix="${outdir}/${prefix}";
     sex=`sh ~/crg/get_XY.sh $reads`;
-    echo  -e "ExpansionHunter --reads $reads --reference $ref --variant-catalog $catalog --output-prefix $eh_prefix --sex $sex \n"
-    ExpansionHunter --reads $reads --reference $ref --variant-catalog $catalog --output-prefix $eh_prefix --sex $sex &
+    echo  -e "COMMAND: ExpansionHunter --reads $reads --reference $ref --variant-catalog $catalog --output-prefix $eh_prefix --sex $sex \n"
+    ExpansionHunter --reads $reads --reference $ref --variant-catalog $catalog --output-prefix $eh_prefix --sex $sex 
 done
 module unload ExpansionHunter/3.0.1
 
 ##EH report per family
 echo "generating multi-sample EH STR report named ${outdir}/${family}_EH_str.tsv"
 
-#following Python scripts requires csv, python-docx
+#following Python script requires csv, python-docx
 #modules I checked didn't have python-docx, so installed in my conda env "str"
 source /hpf/largeprojects/ccmbio/aarthi/miniconda3/etc/profile.d/conda.sh
 conda activate str
 tsv="${outdir}/${family}_EH_str.tsv";
-python ${scripts}/generate_EH_genotype_table.generic.py ${eh_prefix} > $tsv
+echo "COMMAND: python ${scripts}/generate_EH_genotype_table.generic.py ${outdir}"
+python ${scripts}/generate_EH_genotype_table.generic.py ${outdir} > $tsv
 
 #annotate with ref gene, location and repeat size
-python ${scripts}/add_gene+threshold_to_EH_column_headings2.py $tsv ${tr_annot}
 annot="${outdir}/${family}_EH_str.annot.tsv"
-xlsx="${outdir}/${family}_EH_str.annot.xlsx"
-#repeat GT in tsv gets auto-formatted to date in Excel, so convert tsv to xlsx
-python ${scripts}/tsv2xlsx.py $annot $xlsx
+echo "annotating multi-sample EH STR report named $annot"
+echo "COMMAND: python ${scripts}/add_gene+threshold_to_EH_column_headings2.py $tsv ${tr_annot} > $annot"
+python ${scripts}/add_gene+threshold_to_EH_column_headings2.py $tsv ${tr_annot} > $annot
+
+#transpose and seperate columns to final report
+xlsx="${outdir}/${family}_EH_v1.1.xlsx";
+echo "generating family-wise report: $xlsx"
+echo "COMMAND: python ~/crg/eh_sample_report.py ${annot} ${xlsx}"
+python ~/crg/eh_sample_report.py ${annot} ${xlsx}
+
+
+
