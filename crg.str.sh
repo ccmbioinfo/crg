ee83d6da8331aed38bbb162b004f3f4696cedbfa
aarthi-mohan <aarthimohan11@gmail.com>
2021-03-08 14:24:28 -0500

    commendted out EHDN submission

--- crg.str.sh
+++ crg.str.sh
@@ -7,13 +7,14 @@
 
 family=$1;
 
-ppn=`ls bcbio-align/${family}/final/${family}_*/${family}_*-ready.bam | wc -l`;
+#ppn=`ls bcbio-align/${family}/final/${family}_*/${family}_*-ready.bam | wc -l`;
 
 #detect repeats in known sites in each sample, combine and create a annotated xlsx report per family
-eh_job=(qsub crg.eh.sh -v family=$family -l nodes=1:ppn=$ppn);
+eh_job=`qsub ~/crg/crg.eh.sh -v family=$family`;
+echo "Submitted EH for $family: ${eh_job}";
 
 #detect denovo repeats in each sample, combine and create report per family
-ehdn_job=(qsub crg.ehdn.sh -v family=$family -l nodes=1:ppn=$ppn);
+#ehdn_job=(qsub crg.ehdn.sh -v family=$family -l nodes=1:ppn=$ppn);
 
 
 
