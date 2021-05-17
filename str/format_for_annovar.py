d57ad296f8fb7589a1257ee69e493c269aaffec8
aarthi-mohan <aarthimohan11@gmail.com>
2021-05-17 16:19:19 -0400

    split outlier and size column sample-wise

--- str/format_for_annovar.py
+++ str/format_for_annovar.py
@@ -22,9 +22,11 @@ g1k_manifest = sys.argv[3]
 date = ehdn_out.split(".")[-2]
 d = os.path.dirname(dbscan_out)
 outfile = os.path.join(d,"merged.rare.EHdn.expansion." + date + ".tsv")
+samples = []
 print("input files: ", ehdn_out, dbscan_out, g1k_manifest)
 print("out:", outfile)
 
+#read 1000G sample names from manifest file
 g1k = []
 with open(g1k_manifest) as f:
     for i in f:
@@ -33,30 +35,55 @@ with open(g1k_manifest) as f:
             g1k.append(re.split("\t|' '",i)[0])
         else:
             g1k.append(i)
-
-print("length of 1000genomes samples: " , len(g1k), g1k[100:120])
 all_g1k_sample = lambda x: set(x).difference(g1k) 
+
+#read DBSCAN output
 dbscan = namedtuple("dbscan", "chr start end motif outliers key")
 merged_exp = []
 with open(dbscan_out) as f:
+    f.readline()
     for i in f:
         i = i.strip().split("\t")
         key = "#".join(i[0:3])
         outlier = re.split(";",i[4])
-        #print(all_g1k_sample(outlier),outlier)
         if all_g1k_sample(outlier):
-            #print(all_g1k_sample(outlier),outlier)
             merged_exp.append(dbscan(chr=i[0],start=i[1],end=i[2],motif=i[3],outliers=i[4],key=key))
+            for o in outlier:
+                if not o in g1k:
+                    samples.append(o)
 
+samples = list(set(samples))
+print("samples = ", samples)
+#read EHDN output
 ehdn = namedtuple("ehdn", "motif outliers size ref chr start end a1000g key")
 ehdn_exp = []
 with open(ehdn_out) as f:
+    f.readline()
     for i in f:
         i = i.strip().split("\t")
         key = "#".join(i[5:8])
         ehdn_exp.append(ehdn(motif=i[1],outliers=i[2],size=i[3],ref=i[4],chr=i[5],start=i[6],end=i[7],a1000g=i[8],key=key))
 ehdn_exp        
 
+#match reacords from DBSCAN and EHDN to retrieve all info
+#DBSCAN repeats are merged based on locations, so may not match original EHDN output
+
+def split_outlier_size(outlier,size,samples):
+    outlier_l = re.split(";",outlier) 
+    size_l = re.split(";",size)
+    if len(size_l) == len(outlier_l):
+        len_samples = len(samples)
+        outlier_samples = ["0"]*(len_samples+1)
+        outlier_sizes = ["0"]*len_samples
+        for i,s in enumerate(samples):
+            if s in outlier_l:
+                ind = outlier_l.index(s)
+                outlier_samples[i] = "1"
+                outlier_sizes[i] = size_l[ind]
+            else:
+                if s in g1k:
+                    outlier_samples[len_samples] += 1
+    return outlier_samples, outlier_sizes
 
 found, allkey, out, seen = [], [], [], []
 ref, alt = "0", "-"
@@ -66,9 +93,16 @@ for i in merged_exp:
         if i.key == j.key:
             if i.outliers == j.outliers:
                 found.append(i)
-                out.append([i.chr, i.start, i.end, ref, alt, i.motif, i.outliers, i.key, j.size, j.a1000g])
+                outliers, sizes = split_outlier_size(i.outliers, j.size, samples)
+                print(outliers, sizes, i.key)
+                # out.append([i.chr, i.start, i.end, ref, alt, i.motif, i.outliers, i.key, j.size, j.a1000g])
+                # out.append([i.chr, i.start, i.end, ref, alt, i.motif, outliers, sizes, i.key, j.a1000g])
+                o = [i.chr, i.start, i.end, ref, alt, i.motif ] +  outliers + sizes + [i.key, j.a1000g]
+                out.append(o)
+                print(o)
             # seen.append(i)
                 continue
+
 
 notfound = list(set(allkey).difference(found))
 within = lambda a,b,c,d: ( c <= a and a <= d ) or ( c <= b and b <= d)
@@ -79,11 +113,24 @@ for i in notfound:
             r2 = range(int(j.start),int(j.end))
 #            if i.start == j.start or i.end == j.end or len(set(r1).intersection(r2)) > 5:
             if within(i.start,i.end,j.start,j.end) or within(j.start,j.end,i.start,i.end):
-                out.append([i.chr, i.start, i.end, ref, alt, i.motif, i.outliers, i.key, j.size, j.a1000g])
+                # out.append([i.chr, i.start, i.end, ref, alt, i.motif, i.outliers, i.key, j.size, j.a1000g])
+                outliers, sizes = split_outlier_size(i.outliers, j.size, samples)
+                o = [i.chr, i.start, i.end, ref, alt, i.motif ] +  outliers + sizes + [i.key, j.a1000g]
+                # out.append([i.chr, i.start, i.end, ref, alt, i.motif, outliers, sizes, i.key, j.a1000g])
+                out.append(o)
+                print(o)
 #               seen.append(i)
                 continue
 
+columns = ["chr", "start", "end", "ref", "alt", "motif", ]
+columns += ["outlier." + s for s in samples]
+columns += ["1000G_outlier_count"]
+columns += ["size." + s for s in samples]
+columns += ["key", "1000G_freq_perc"]
 with open(outfile, "w")  as f:
+    for i in columns:
+        f.writelines("%s\t"%i)
+    f.writelines("\n")
     for i in out:
         for j in i:
             f.writelines("%s\t"%j)
